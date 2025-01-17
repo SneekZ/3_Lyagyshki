c60710294ca6da2deceafe47b52a172cbb3d14ae
roman-vista <sindeevr5@gmail.com>
2025-01-17 10:26:37 +0300

    Немного доработана страница 404

--- frontend/src/components/NotFound/NotFound.jsx
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -1,5 +1,19 @@
-export default function NotFound() {
-    return (
-      <div>Страница не найдена</div>
-    )  
-}
+import React from 'react';
+import { QRCode } from 'antd';
+import headsetIcon from "./src/headset.png"
+
+const NotFound = () => {
+
+  return (
+    <div className="notfound_container">
+      <QRCode
+        size={420}
+        errorLevel='H'
+        value="https://youtu.be/YIBV4AnKKgU?si=y11hD3meD3Ic5qJQ"
+        icon={ headsetIcon }
+        color="white"/>
+    </div>
+  );
+};
+
+export default NotFound;
